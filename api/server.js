6b3bdcbdf562488950f45b3b7b670b1e99df5c62
JustBeGabs <justbegabs@gmail.com>
2025-04-29 17:09:21 -0300

    porta render

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
